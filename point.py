2c7188b65ba7a585791dc2e570f148d58d314736
logotip <logotip123@yahoo.com>
2019-12-05 10:35:33 +0200

    v 1.0

--- point.py
+++ point.py
@@ -1,2 +1,19 @@
+"""module with class point"""
+
 class Point:
-    pass
+    """class to represent point in 2D space"""
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def __repr__(self):
+        return f"Point({self._x}, {self._y})"
+
+    def __str__(self):
+        return f"({self._x}, {self._y})"
+
+    def __eq__(self, other):
+        return self._x == other._x and self._y == other._y
+
+    def __hash__(self):
+        return hash(self._x + self._y)
