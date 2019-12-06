b851fd2f7517bead17ae18cb0165ece858ff11c5
Dima <dima.sapsay@gmail.com>
2019-12-06 10:31:35 +0200

    third commit

--- point.py
+++ point.py
@@ -6,18 +6,18 @@ Create class to represent point in 2D space. Don't use dataclasses.
 
 class Point:
     """Class to represent point in 2D space"""
-    def __init__(self, x_axis, y_axis):
-        self.x_axis = x_axis
-        self.y_axis = y_axis
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
 
     def __repr__(self):
-        return "Point({}, {})".format(self.x_axis, self.y_axis)
+        return "Point({}, {})".format(self.x_coordinate, self.y_coordinate)
 
     def __str__(self):
-        return "({}, {})".format(self.x_axis, self.y_axis)
+        return "({}, {})".format(self.x_coordinate, self.y_coordinate)
 
     def __eq__(self, other):
-        return self.x_axis == other.x_axis and self.y_axis == other.y_axis
+        return self.x_coordinate == other.x_coordinate and self.y_coordinate == other.y_coordinate
 
     def __hash__(self):
-        return hash(self.x_axis)
+        return hash((self.x_coordinate, self.y_coordinate))
